53f4cf582a7627ececd8281080ef6b08d03e9784
KrishnoMD305 <krishnomd60@gmail.com>
2025-10-19 23:11:14 +0600

    Updating Pharmacy Class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 
-
 using namespace std;
 
 
 int main(){
-    
+    cout << "\n";
+    cout << "╔════════════════════════════════════════════════════════╗" << endl;
+    cout << "║                                                        ║" << endl;
+    cout << "║         WELCOME TO PHARMACY MANAGEMENT SYSTEM          ║" << endl;
+    cout << "║                                                        ║" << endl;
+    cout << "╚════════════════════════════════════════════════════════╝" << endl;
+    cout << "\n";
     return 0;
 }
